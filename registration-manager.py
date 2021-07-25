d8402297f05b454a50ae40f536e4d78e6f10156e
Dread Pirate Robot <robotlandscape@gmail.com>
2021-07-25 16:58:51 -0400

    Much more added. ^C to exit. Printing all data works

--- registration-manager.py
+++ registration-manager.py
@@ -1,8 +1,9 @@
+#!/usr/bin/python
 import csv 
 
-def init():
-    raw_data = open("battle_royale.csv", "r")
-    address = csv.reader(raw_data)
+raw_data = open("battle_royale.csv", "r")
+address = csv.reader(raw_data)
+
 
 def prompt():
     # options
@@ -10,15 +11,36 @@ def prompt():
     print("\tA: Find pre-registered player")
     print("\tB: Find the number of a specific player")
     print("\tC: Print list of players")
-    print("\tQ: Quit/Log Out")
-
-    
+    print("\tDo ^C (Ctrl c) at any time to exit.")
     return input("\t > ").lower()
+
+
+def number_to_player():
+    print("Enter a number (NYI)")
+    return 0
+
+
+def player_to_number():
+    print("Search for a player name! (NYI)")
+    return 0
+
+
+def print_players():
+    for line in address:
+        print(line)
+
 
 def main():
     print("Welcome to the Battle Royale Game Tournament Registration!")
-    init()
-    choice = prompt()
-    print(choice)
+    try: 
+        choice = prompt()
+        if choice == 'a':
+            number_to_player()
+        if choice == 'b':
+            player_to_number()
+        if choice == 'c':
+            print_players()
+    except KeyboardInterrupt:
+        print("\nBye!")
 
 main()
